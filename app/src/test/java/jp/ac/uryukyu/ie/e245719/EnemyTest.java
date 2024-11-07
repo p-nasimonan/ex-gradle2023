d6b1b8f52ef4643fb7f80ebadb5ff826cd8f1120
p-nasi <pnasimonan@gmail.com>
2024-11-07 14:15:40 +0900

    check test-code

--- app/src/test/java/jp/ac/uryukyu/ie/e245719/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e245719/EnemyTest.java
@@ -1,7 +1,7 @@
-package jp.ac.uryukyu.ie.tnal;
+package jp.ac.uryukyu.ie.e245719;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class EnemyTest {
     /**
